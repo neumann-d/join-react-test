1ebbc9e089bab070d1e14212735c90f1c7bd97e7
D. Neumann <daniel.neumann@jobai.de>
2020-04-25 23:44:58 +0200

    add simple jest test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
+import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import React from 'react';
+import { Router } from 'react-router-dom';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+import App from './App';
+
+test('renders candidate/recruiter tab links', () => {
+    const history = createMemoryHistory();
+    render(
+        <Router history={history}>
+            <App />
+        </Router>
+    );
+
+    const candidateTab = document.getElementById('candidate-tab') || document.createElement('a');
+    const recruiterTab = document.getElementById('recruiter-tab') || document.createElement('a');
+
+    expect(candidateTab.closest('a')).toHaveAttribute('href', '/candidate');
+    expect(recruiterTab.closest('a')).toHaveAttribute('href', '/recruiter');
+});
