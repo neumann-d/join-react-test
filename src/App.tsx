24a8e5853883e90c75c8c5b3c997c45abd931a23
D. Neumann <daniel.neumann@jobai.de>
2020-04-25 23:23:55 +0200

    basic setup for eslint/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import AppBar from "@material-ui/core/AppBar";
-import Avatar from "@material-ui/core/Avatar";
-import { makeStyles } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    marginLeft: theme.spacing(2),
-  }
+    title: {
+        marginLeft: theme.spacing(2),
+    },
 }));
 
 const App = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Avatar alt="Logo" src="/logo192.png" />
-          <Typography variant="h6" className={classes.title}>
-            {process.env.REACT_APP_NAME} {process.env.REACT_APP_VERSION}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Typography variant="h4" component="h1">
-        Hello World
-      </Typography>
-    </>
-  );
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Avatar alt="Logo" src="/logo192.png" />
+                    <Typography variant="h6" className={classes.title}>
+                        {process.env.REACT_APP_NAME} {process.env.REACT_APP_VERSION}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Typography variant="h4" component="h1">
+                Coming soon...
+            </Typography>
+        </>
+    );
 };
 
 export default App;
